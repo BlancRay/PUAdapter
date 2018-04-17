05a42d2afe3931ab89f2cbcb1bb876fd6376b856
xuleiboy1234 <xuleiboy1234@gmail.com>
2018-04-17 10:08:38 +0800

    test new method for build model

--- dmp-private-tag/src/test/scala/offline/unit_test.scala
+++ dmp-private-tag/src/test/scala/offline/unit_test.scala
@@ -5,73 +5,78 @@ import java.io.{FileOutputStream, PrintWriter}
 import org.apache.spark.mllib.tree.model.RandomForestModel
 import org.apache.spark.{SparkConf, SparkContext}
 
-import scala.io.Source
-
 object unit_test {
-  val pwd = "E:\\xulei\\zhiziyun\\model\\test\\test7\\"
-  val mode = "test" // train or test
-  def main(args: Array[String]): Unit = {
-    println("开始模型测试")
-    //设置hadoop目录
-    System.setProperty("hadoop.home.dir", "E:\\xulei\\hadoop2.6.0")
-    val sc = new SparkContext(new SparkConf().setAppName("RandomForestClassificationTest").setMaster("local[4]"))
-    println("加载模型及参数")
-    val model = RandomForestModel.load(sc, pwd + "model")
-    val estC = Source.fromFile(pwd + "estC").getLines().next().toDouble
-    println(estC)
-    var sb = new StringBuffer()
-    //    LOG.info("开始计算模型影响因子")
-    val feature_importance = importance.importance(model.trees, 2000)
+    val pwd = "E:\\xulei\\zhiziyun\\model\\test\\test1\\"
+    val mode = "test" // train or test
+    def main(args: Array[String]): Unit = {
+        println("开始模型测试")
+        //设置hadoop目录
+        System.setProperty("hadoop.home.dir", "E:\\xulei\\hadoop2.6.0")
+        val sc = new SparkContext(new SparkConf().setAppName("RandomForestClassificationTest").setMaster("local[4]"))
+        println("加载模型及参数")
+        //            val model = RandomForestModel.load(sc, pwd + "model")
+        val model = RandomForestModel.load(sc, pwd + "finalmodel")
+        //            val estC = Source.fromFile(pwd + "estC").getLines().next().toDouble
+        val estC = 1.0
+        println(estC)
+        var sb = new StringBuffer()
+        //    LOG.info("开始计算模型影响因子")
+        val feature_importance = importance.importance(model.trees, 2000)
 
-    //    val feature_importance = importance.featureImportances(model.trees, 2000)    //second method to get feature importance
-    sb.append("tagIndex").append(",").append("intensity").append("\n")
-    feature_importance.keys.foreach { i =>
-      sb.append(i.toString)
-        .append(",")
-        .append(feature_importance(i).toString)
-        .append("\n")
+        //    val feature_importance = importance.featureImportances(model.trees, 2000)    //second method to get feature importance
+        sb.append("tagIndex").append(",").append("intensity").append("\n")
+        feature_importance.keys.foreach { i =>
+            sb.append(i.toString)
+                .append(",")
+                .append(feature_importance(i).toString)
+                .append("\n")
+        }
+        //        write2file(sb, pwd + "importance.csv")
+        write2file(sb, pwd + "importance_finalmodel.csv")
+
+        //    LOG.info("写入模型影响因子")
+        //    tool.postArrayToURL(modelid, prop.getProperty("influence"), importance_mapArray)
+        //
+        //    LOG.info("开始计算相似度")
+        val proba = model_test.evaluate(model, estC, sc, 2000, pwd + mode + "_All").collect()
+        sb = new StringBuffer()
+        //    sb.append("gid,prob\n")
+
+        var nbpos = 0
+        if (mode == "train")
+            nbpos = 700
+        else nbpos = 300
+        for (i <- proba.indices) {
+            if (i < nbpos) // train=700 test=300
+                sb.append(1).append(",").append(proba(i)).append("\n")
+            else
+                sb.append(0).append(",").append(proba(i)).append("\n")
+        }
+        //        write2file(sb, pwd + "proba_" + mode + "_all.csv")
+        write2file(sb, pwd + "proba_" + mode + "_all_finalmodel.csv")
+        //    tool.log(modelid, "生成相似度与个体数量关系", "1", prop.getProperty("log"))
+        val start = System.currentTimeMillis()
+        val similar = similarity.statistics(proba, 0.001, 1).toMap
+        val end = System.currentTimeMillis()
+        println(end - start)
+        //
+        //    //callBackSimilar
+        sb = new StringBuffer()
+        sb.append("similar,num\n")
+        similar.keys.foreach { i =>
+            sb.append(i.toString)
+                .append(",")
+                .append(similar(i).toString)
+                .append("\n")
+        }
+        //        write2file(sb, pwd + "similarity_" + mode + "_all.csv")
+        write2file(sb, pwd + "similarity_" + mode + "_all_finalmodel.csv")
+        sb = null
     }
-    write2file(sb, pwd + "importance.csv")
 
-    //    LOG.info("写入模型影响因子")
-    //    tool.postArrayToURL(modelid, prop.getProperty("influence"), importance_mapArray)
-    //
-    //    LOG.info("开始计算相似度")
-    val proba = model_test.evaluate(model, estC, sc, 2000, pwd + mode + "_All").collect()
-    sb = new StringBuffer()
-    //    sb.append("gid,prob\n")
-
-    var nbpos = 0
-    if (mode == "train")
-      nbpos = 700
-    else nbpos = 300
-    for (i <- proba.indices) {
-      if (i < nbpos) // train=700 test=300
-        sb.append(1).append(",").append(proba(i)).append("\n")
-      else
-        sb.append(0).append(",").append(proba(i)).append("\n")
+    def write2file(string: StringBuffer, dir: String): Unit = {
+        val writer = new PrintWriter(new FileOutputStream(dir))
+        writer.print(string)
+        writer.close()
     }
-    write2file(sb, pwd + "proba_" + mode + "_all.csv")
-
-    //    tool.log(modelid, "生成相似度与个体数量关系", "1", prop.getProperty("log"))
-    val similar = similarity.statistics(proba, 0.001, 1).toMap
-    //
-    //    //callBackSimilar
-    sb = new StringBuffer()
-    sb.append("similar,num\n")
-    similar.keys.foreach { i =>
-      sb.append(i.toString)
-        .append(",")
-        .append(similar(i).toString)
-        .append("\n")
-    }
-    write2file(sb, pwd + "similarity_" + mode + "_all.csv")
-    sb = null
-  }
-
-  def write2file(string: StringBuffer, dir: String): Unit = {
-    val writer = new PrintWriter(new FileOutputStream(dir))
-    writer.print(string)
-    writer.close()
-  }
 }
--- dmp-private-tag/src/test/scala/offline/unit_train.scala
+++ dmp-private-tag/src/test/scala/offline/unit_train.scala
@@ -13,86 +13,132 @@ import scala.collection.mutable
 import scala.io.Source
 
 object unit_train {
-  val pwd = "E:\\xulei\\zhiziyun\\model\\test\\test7\\"
-  def main(args: Array[String]): Unit = {
-    println("调用算法训练模型")
-    //设置hadoop目录
-    System.setProperty("hadoop.home.dir", "E:\\xulei\\hadoop2.6.0")
-    val sc = new SparkContext(new SparkConf().setAppName("RandomForestClassificationTrain").setMaster("local[4]"))
-    val p = readData(sc, sc.textFile(pwd + "train_P_label").collect(), "P")
-    val u = readData(sc, sc.textFile(pwd + "train_U").collect(), "U")
+    val pwd = "E:\\xulei\\zhiziyun\\model\\test\\test1\\"
 
-    val categoryInfos = Source.fromFile(pwd + "attrInfo.txt").getLines
-    val categoryInfo = mutable.Map[Int, Int]()
-    categoryInfos.foreach { each =>
-      val tmp = each.split("\t")
-      categoryInfo.put(tmp(0).toInt, tmp(1).toInt)
+    def main(args: Array[String]): Unit = {
+        println("调用算法训练模型")
+        //设置hadoop目录
+        System.setProperty("hadoop.home.dir", "E:\\xulei\\hadoop2.6.0")
+        val sc = new SparkContext(new SparkConf().setAppName("RandomForestClassificationTrain").setMaster("local[4]"))
+        val p = readData(sc, sc.textFile(pwd + "train_P_label").collect(), "P")
+        val u = readData(sc, sc.textFile(pwd + "train_U").collect(), "U")
+
+        val categoryInfos = Source.fromFile(pwd + "attrInfo.txt").getLines
+        val categoryInfo = mutable.Map[Int, Int]()
+        categoryInfos.foreach { each =>
+            val tmp = each.split("\t")
+            categoryInfo.put(tmp(0).toInt, tmp(1).toInt)
+        }
+
+        //    //train model use p and u,return arg c and model
+        val (estC, model) = fit(p, u, categoryInfo.toMap)
+        /*
+        test after get estC, use new U data to build Model
+         */
+        println("test start")
+        val test = u.collect()
+        val proba = evaluate(model, estC, sc, 2000, u).collect()
+        val newU = new Array[LabeledPoint](test.length)
+        for (i <- proba.indices) {
+            var label = 0.0
+            if (proba(i) >= 0.5)
+                label = 1.0
+            else
+                label = 0.0
+            newU.update(i, LabeledPoint(label, test(i).features))
+        }
+        val newRddU = sc.makeRDD(newU)
+        var finalModel: RandomForestModel = null
+        println("rebuilding")
+        finalModel = RandomForest.trainClassifier(p.union(newRddU).union(newRddU), 2, categoryInfo.toMap, 50, "auto", "gini", 25, 200)
+        dirDel(new File(pwd + "finalmodel"))
+        finalModel.save(sc, pwd + "finalmodel")
+        println("test end")
+        /*
+        test end
+         */
+        //
+        //    LOG.info("模型训练完成，正在保存模型")
+        val saveC = new PrintWriter(new FileOutputStream(pwd + "estC"))
+        saveC.write(estC.toString)
+        saveC.close()
+        val modelFile = new File(pwd + "model")
+        dirDel(modelFile)
+        model.save(sc, pwd + "model")
+
     }
 
-    //    //train model use p and u,return arg c and model
-    val (estC, model) = fit(p, u, categoryInfo.toMap)
-    //
-    //    LOG.info("模型训练完成，正在保存模型")
-    val saveC = new PrintWriter(new FileOutputStream(pwd + "estC"))
-    saveC.write(estC.toString)
-    saveC.close()
-    val modelFile = new File(pwd + "model")
-    dirDel(modelFile)
-    model.save(sc, pwd + "model")
-
-  }
-
-  def dirDel(path: File) {
-    if (!path.exists())
-      return
-    else if (path.isFile) {
-      path.delete()
-      println(path + ":  文件被删除")
-      return
+    def dirDel(path: File) {
+        if (!path.exists())
+            return
+        else if (path.isFile) {
+            path.delete()
+            println(path + ":  文件被删除")
+            return
+        }
+        val file: Array[File] = path.listFiles()
+        for (d <- file) {
+            dirDel(d)
+        }
+        path.delete()
+        println(path + ":  目录被删除")
     }
-    val file: Array[File] = path.listFiles()
-    for (d <- file) {
-      dirDel(d)
+
+    def readData(sc: SparkContext, data: Array[String], dtype: String): RDD[LabeledPoint] = {
+        val lp = new Array[LabeledPoint](data.length)
+        var i = 0
+        data.foreach { eachdata =>
+            val features = eachdata.split(",")
+            val feature = new Array[Double](features.length) //6868
+            for (j <- feature.indices) {
+                feature(j) = features(j).toDouble
+            }
+            val dv: Vector = Vectors.dense(feature)
+            if (dtype == "P")
+                lp.update(i, LabeledPoint(1, dv))
+            else
+                lp.update(i, LabeledPoint(0, dv))
+            i += 1
+        }
+        sc.makeRDD(lp)
     }
-    path.delete()
-    println(path + ":  目录被删除")
-  }
 
-  def readData(sc: SparkContext, data: Array[String], dtype: String): RDD[LabeledPoint] = {
-    val lp = new Array[LabeledPoint](data.length)
-    var i = 0
-    data.foreach { eachdata =>
-      val features = eachdata.split(",")
-      val feature = new Array[Double](features.length) //6868
-      for (j <- feature.indices) {
-        feature(j) = features(j).toDouble
-      }
-      val dv: Vector = Vectors.dense(feature)
-      if (dtype == "P")
-        lp.update(i, LabeledPoint(1, dv))
-      else
-        lp.update(i, LabeledPoint(0, dv))
-      i += 1
+
+    def fit(POS: RDD[LabeledPoint], UNL: RDD[LabeledPoint], categoryInfo: Map[Int, Int]): (Double, RandomForestModel) = {
+        val hold_out_ratio = 0.2
+        var c = Double.NaN
+        var model_hold_out: RandomForestModel = null
+        val splits = POS.randomSplit(Array(hold_out_ratio, 1.0 - hold_out_ratio))
+        val (p_test, p_train) = (splits(0), splits(1))
+        // Train a RandomForest model.
+        val trainData = p_train.union(UNL)
+
+        model_hold_out = RandomForest.trainClassifier(trainData, 2, categoryInfo, 50, "auto", "gini", 25, 200)
+        val hold_out_predictions = tool.predict(p_test, model_hold_out)
+        c = hold_out_predictions.sum() / hold_out_predictions.count()
+        if (c.isNaN) {
+            println("C is Nan")
+        }
+        (c, model_hold_out)
     }
-    sc.makeRDD(lp)
-  }
 
+    def evaluate(model: RandomForestModel, estC: Double, sc: SparkContext, nbTagFeatures: Int, test_data: RDD[LabeledPoint]): RDD[Double] = {
+        //    tool.log(modelid, "生成模型测试数据", "1", prop.getProperty("log"))
+        //    val (_, test_data) = tool.read_convert(modelid, "N_SOURCE", sc, nbTagFeatures)
 
-  def fit(POS: RDD[LabeledPoint], UNL: RDD[LabeledPoint], categoryInfo: Map[Int, Int]): (Double, RandomForestModel) = {
-    val hold_out_ratio = 0.2
-    var c = Double.NaN
-    var model_hold_out: RandomForestModel = null
-    val splits = POS.randomSplit(Array(hold_out_ratio, 1.0 - hold_out_ratio))
-    val (p_test, p_train) = (splits(0), splits(1))
-    // Train a RandomForest model.
-    val trainData = p_train.union(UNL)
-
-    model_hold_out = RandomForest.trainClassifier(trainData, 2, categoryInfo, 50, "auto", "gini", 25, 200)
-    val hold_out_predictions = tool.predict(p_test, model_hold_out)
-    c = hold_out_predictions.sum() / hold_out_predictions.count()
-    if (c.isNaN) {
-      println("C is Nan")
+        println("N_SOURCE数量:" + test_data.count().toString)
+        //    tool.log(modelid, "模型测试中", "1", prop.getProperty("log"))
+        val prediction = predict(test_data, model)
+        val proba = prediction.map(_ / estC)
+        //    tool.log(modelid, "模型测试完成", "1", prop.getProperty("log"))
+        proba
     }
-    (c, model_hold_out)
-  }
+
+    def predict(points: RDD[LabeledPoint], model: RandomForestModel): RDD[Double] = {
+        val numTrees = model.trees.length
+        val trees = points.sparkContext.broadcast(model.trees)
+        points.map { point =>
+            trees.value.map(_.predict(point.features)).sum / numTrees
+        }
+    }
 }
